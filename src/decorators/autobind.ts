19443d3441feb756619f7c255171bda7fb9b0577
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2020-12-19 00:06:31 -0300

    using esmodules

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,17 +1,15 @@
-namespace App {
-    // autobind decorator
-    export function autobind(
-        _: any,
-        _2: string,
-        descriptor: PropertyDescriptor
-    ) {
-        const originalMethod = descriptor.value
-        const adjDescriptor: PropertyDescriptor = {
-            configurable: true,
-            get() {
-                return originalMethod.bind(this)
-            }
+// autobind decorator
+export function autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this)
         }
-        return adjDescriptor
     }
+    return adjDescriptor
 }
