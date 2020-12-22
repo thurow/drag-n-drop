e1439c0412facf35f83e16f738eae53b0cbdc4f7
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2020-12-22 00:04:10 -0300

    finished development workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
-module.exports = {
+module.exports = () => ({
+    mode: 'development',
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        inline: true,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+        port: 3005
     },
     devtool: 'inline-source-map',
     module: {
@@ -18,5 +29,12 @@ module.exports = {
     },
     resolve: {
         extensions: ['.ts', '.js']
-    }
-}
+    },
+    target: 'web',
+    plugins: [
+        new HtmlWebPackPlugin({
+            inject: true,
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+})
